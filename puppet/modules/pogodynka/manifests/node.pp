adc52aeb5f658258fd72459a81fa0a7367a874e8
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-04-29 20:05:02 +0200

    Adding required modules for tomcat/postgresql. Draft of tomcat configuration

--- puppet/modules/pogodynka/manifests/node.pp
+++ puppet/modules/pogodynka/manifests/node.pp
@@ -2,4 +2,22 @@ class pogodynka::node {
   notify {
     "it works":
   }
+  
+  tomcat::install {
+    '/opt/tomcat8.5':
+      source_url => 'http://mirror.23media.de/apache/tomcat/tomcat-8/v8.5.14/bin/apache-tomcat-8.5.14.tar.gz';
+  }
+
+  tomcat::instance {
+    'tomcat8.5-production':
+      catalina_home => '/opt/tomcat8.5',
+      catalina_base => '/opt/tomcat8.5/production';
+  }
+
+  tomcat::config::server {
+    'tomcat8.5-production':
+      catalina_base => '/opt/tomcat8.5/production',
+      port => '80';
+  }
 }
+
