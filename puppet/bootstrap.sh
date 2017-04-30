c214d3326dab93ea7bf2451247d4d2889b78545a
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-04-30 10:31:38 +0200

    Small changes to bootstrap script

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# wget https://raw.githubusercontent.com/SamouczekProgramisty/Pogodynka/master/puppet/bootstrap.sh
+# chmod +x bootstrap.sh
+# ./bootstrap.sh
+
 CONFIGURATION_DIR=/opt/configuration/pogodynka
 
 apt-get update
 apt-get install puppet libssl-dev git -y
 
 git clone https://github.com/SamouczekProgramisty/Pogodynka.git $CONFIGURATION_DIR
+cd $CONFIGURATION_DIR
+git submodule init
+git submodule update
 
-cd $CONFIGURATION_DIR/puppet
-puppet apply --modulepath=$CONFIGURATION_DIR/puppet/modules/ manifests/site.pp
+puppet apply --modulepath=$CONFIGURATION_DIR/puppet/modules/ $CONFIGURATION_DIR/puppet/manifests/site.pp
