a6bca9ba916cff0303b5380bde32a27a426fa8f8
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-04-30 11:22:28 +0200

    Adding gpg key for webupd8 java apt list

--- puppet/modules/pogodynka/manifests/java.pp
+++ puppet/modules/pogodynka/manifests/java.pp
@@ -7,16 +7,20 @@ class pogodynka::java {
       path    => '/etc/apt/sources.list.d/webupd8team-java.list',
       content => "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main\ndeb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main";
 
+    'java-apt-key':
+      path    => '/etc/apt/trusted.gpg.d/webupd8.gpg',
+      content => 'puppet://modules/pogodynka/webupd8.gpg';
+
     $responsefile:
       ensure  => 'present',
-      content => "${package} shared/accepted-oracle-license-v1-1 select true",
+      content => "${package} shared/accepted-oracle-license-v1-1 select true";
   }
   
   package {
     'oracle-java8-installer':
       ensure       => 'latest',
       responsefile => $responsefile,
-      require      => [File['java-apt-list'], Class['apt::update']];
+      require      => [File['java-apt-list'], File['java-apt-key'], Class['apt::update']];
 
     'oracle-java8-set-default':
       ensure  => 'latest',
