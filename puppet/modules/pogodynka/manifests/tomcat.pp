b3a9bcffc2f6f75b058e354806a839da4959f6f0
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-05-21 22:41:25 +0200

    Fixing mandatory syntax errors

--- puppet/modules/pogodynka/manifests/tomcat.pp
+++ puppet/modules/pogodynka/manifests/tomcat.pp
@@ -1,6 +1,8 @@
 class pogodynka::tomcat {
+
+  $code_dir = "/opt/configuration/pogodynka"
+  $war_file = "${code_dir}/datavault/build/libs/datavault-1.0-SNAPSHOT.war"
   $catalina_home = '/opt/tomcat8.5'
-  $catalina_base = "${catalina_home}/production"
 
   tomcat::install {
     '/opt/tomcat8.5':
@@ -10,11 +12,8 @@ class pogodynka::tomcat {
   tomcat::instance {
     'tomcat8.5-production':
       catalina_home => $catalina_home,
-      catalina_base => $catalina_base;
+      catalina_base => $catalina_home;
   }
-
-  $code_dir = "/opt/configuration/pogodynka";
-  $war_file = "${code_dir}/datavault/build/libs/datavault-1.0-SNAPSHOT.war";
 
   exec {
     'build_war':
@@ -23,10 +22,10 @@ class pogodynka::tomcat {
   }
 
   file {
-    "${catalina_base}/webapps/datavault.war":
-      source   => "file://${war_file}"
-      requires => [Exec['build_war'], Tomcat::Instance['tomcat8.5-production']];
-      group    => 'tomcat',
-      owner    => 'tomcat';
+    "${catalina_home}/webapps/datavault.war":
+      source  => "file://${war_file}",
+      require => [Exec['build_war'], Tomcat::Instance['tomcat8.5-production']],
+      group   => 'tomcat',
+      owner   => 'tomcat';
   }
 }
