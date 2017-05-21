e67d0ee4f333cb30b5936b76c2f563fc634d2ec2
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-05-21 21:48:50 +0200

    Fixing typo in nginx configuration

--- puppet/modules/pogodynka/manifests/nginx.pp
+++ puppet/modules/pogodynka/manifests/nginx.pp
@@ -1,10 +1,9 @@
 class pogodynka::nginx {
   class {
     '::nginx':
-  }->
+  } ->
   file {
-    '/etc/nginx/sites-enables/pogodynka.pietraszek.pl':
-      source => 'puppet:///modules/pogodynka/nginx.conf',
-      notify => Service['nginx'];
+    '/etc/nginx/sites-enabled/pogodynka.pietraszek.pl':
+      source => 'puppet:///modules/pogodynka/nginx.conf';
   }
 }
